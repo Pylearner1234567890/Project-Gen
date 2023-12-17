929efd0de01965eb1131169bfaa40e71b78d1c4c
Pylearner1234567890 <144495215+Pylearner1234567890@users.noreply.github.com>
2023-12-17 11:33:43 -0600

    Update error check.py

--- PROJECT GEN/error check.py
+++ PROJECT GEN/error check.py
@@ -1,11 +1,11 @@
-# the error_check 1.7 system is a very shitty way to check if something is wrong
+# the error_check 1.8  system is a very shitty way to check if something is wrong
 import time
 import pygame
 # 0 = success 1 = fail
 # THIS IS A SCRIPT
 errorCHECK = int(input('is something wrong? For yes type 1, For No type 0:'))
 if errorCHECK == 1:
-    print("1 FOUND SOMETHING IS WRONG")
+    print("1 FOUND SOMETHING IS WRONG pls report this error to https://shorturl.at/uLVW0")
 if errorCHECK == 0:
     print("0 CODE SUCCESS NOTHING IS WRONG")
 if errorCHECK != 1 and errorCHECK != 0:
@@ -24,6 +24,6 @@ if errorCHECK != 1 and errorCHECK != 0:
     print("SYSTEM OVERLOAD ANSWERS NOT HELPING.")
     print("PLEASE WAIT FOR SYSTEM REPAIR UNIT TO RESPOND")
     time.sleep(20)
-    print("SYSTEM REPAIR UNIT: expected actual answers instead got ", errorCHECK, " Next time Input 1 or 0. THANK YOU")
-    # I made this entire shit for a goof, so I made a more very nice way of saying Hey dumbass put an actual answer
+    print("SYSTEM REPAIR UNIT: expected actual answers instead got:", errorCHECK, "Next time Input 1 or 0. THANK YOU")
+    # I made this entire shit for a goof, so I made a more very nice way of saying Hey dumb ass put an actual answer
 # 6A-61-79-20-62-65-61-6E this is nothing :>
